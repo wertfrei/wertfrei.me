f96df6c6b2b515aa1b00739e9eddfbb9c67c25af
MathisBullinger <mathis.bullinger@code.berlin>
2019-12-28 20:50:27 +0100

    feat: dynamically render single value answers

--- src/pages/Home/Canvas.tsx
+++ src/pages/Home/Canvas.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect, MutableRefObject } from 'react'
 import styled from 'styled-components'
 import { useCanvasSize } from '../../utils/hooks'
+import slides from '../../slides.json'
 
 interface Props {
   slide: number
@@ -29,19 +30,46 @@ function useRender(ref: MutableRefObject<HTMLCanvasElement>) {
     if (!ctx) return
     ctx.clearRect(0, 0, width, height)
     ctx.fillStyle = '#ff0739'
-    console.log('render', slide)
     ctx.beginPath()
-    if (slide === 0) {
-      ctx.moveTo(0, height)
-      ctx.lineTo(0, (height / 4) * 3)
-      ctx.lineTo(width - width / 5, 0)
-      ctx.lineTo(width, 0)
-      ctx.lineTo(width, height)
-    } else if (slide === 1) {
-      ctx.moveTo(width / 2, height)
-      ctx.lineTo(width, height / 2)
-      ctx.lineTo(width, height)
-    } else {
+
+    function renderSingle(v: number) {
+      const A = 29.3 * (Math.PI / 180)
+      const h1 = height * v - (1 / 2) * width * Math.tan(A)
+      const h2 = (width * Math.sin(A)) / Math.sin(90 * (Math.PI / 180) - A)
+      if (h1 > 0 && h1 + h2 <= height) {
+        ctx.moveTo(0, height - h1)
+        ctx.lineTo(width, height - (h1 + h2))
+        ctx.lineTo(width, (1 - slides[0].value) * height)
+        ctx.lineTo(width, height)
+        ctx.lineTo(0, height)
+      } else if (h1 <= 0) {
+        const w =
+          (Math.sqrt(2) * Math.sqrt(height) * Math.sqrt(width) * Math.sqrt(v)) /
+          Math.sqrt(Math.tan(A))
+        const h = (w * Math.sin(A)) / Math.sin(90 * (Math.PI / 180) - A)
+        ctx.moveTo(width - w, height)
+        ctx.lineTo(width, height - h)
+        ctx.lineTo(width, height)
+      } else {
+        const ai = 90 * (Math.PI / 180) - A
+        let h =
+          (Math.sqrt(2) *
+            Math.sqrt(height) *
+            Math.sqrt(width) *
+            Math.sqrt(1 - v)) /
+          Math.sqrt(Math.tan(ai))
+        let w = (h * Math.sin(ai)) / Math.sin(90 * (Math.PI / 180) - ai)
+        ctx.moveTo(0, h)
+        ctx.lineTo(w, 0)
+        ctx.lineTo(width, 0)
+        ctx.lineTo(width, height)
+        ctx.lineTo(0, height)
+      }
+    }
+
+    if (slide < slides.length && 'value' in slides[slide])
+      renderSingle(slides[slide].value)
+    else {
       ctx.moveTo(width / 4, (height / 4) * 3)
       ctx.lineTo(width / 2, height / 4)
       ctx.lineTo((width / 4) * 3, (height / 4) * 3)
