c9bb6985e6334492e45f677d939a076ccb7c3a76
MathisBullinger <mathis.bullinger@code.berlin>
2019-12-29 22:54:08 +0100

    feat: area chart cursor label

--- src/pages/Home/Label/Area.tsx
+++ src/pages/Home/Label/Area.tsx
@@ -3,6 +3,8 @@ import { useMouseX } from '../../../utils/hooks'
 import styled from 'styled-components'
 import slides from '../../../slides.json'
 
+const { format } = new Intl.NumberFormat('de-DE', { style: 'decimal' })
+
 export default function Area({ slide }) {
   const [maxY, setMaxY] = useState(0)
   const x = useMouseX()
@@ -12,29 +14,32 @@ export default function Area({ slide }) {
     setMaxY(Math.max(...values.map(([, v]) => v)))
   }, [values])
 
+  const cm = Math.round(
+    ((x - (window.innerWidth / 5) * 2) / ((window.innerWidth / 5) * 3)) *
+      (values[values.length - 1][0] - values[0][0]) +
+      values[0][0]
+  )
+  const num = ((slides[slide].values as [number, number][]).find(
+    ([v]) => v === cm
+  ) || [0, 0])[1]
+
   if (x < (window.innerWidth / 5) * 2) return null
-  return <S.Cursor hidden pos={x} slide={slide} values={values} maxY={maxY} />
+  return (
+    <>
+      <S.Label>
+        {num}
+        <span>{format(cm / 100)}m</span>
+      </S.Label>
+      <S.Cursor hidden pos={x} normY={num / maxY} />
+    </>
+  )
 }
 
 const S = {
-  Cursor: styled.div.attrs(({ pos, slide, values, maxY }) => ({
+  Cursor: styled.div.attrs(({ pos, normY }) => ({
     style: {
       left: `${pos}px`,
-      top: `${window.innerHeight -
-        (((((slides[slide].values as [number, number][]).find(
-          ([cm]) =>
-            cm ===
-            Math.round(
-              ((pos - (window.innerWidth / 5) * 2) /
-                ((window.innerWidth / 5) * 3)) *
-                (values[values.length - 1][0] - values[0][0]) +
-                values[0][0]
-            )
-        ) || [0, 0])[1] /
-          maxY) *
-          window.innerHeight) /
-          5) *
-          2}px`,
+      top: `${window.innerHeight - normY * ((window.innerHeight / 5) * 2)}px`,
     },
   }))`
     position: absolute;
@@ -46,5 +51,18 @@ const S = {
     transform: translateX(-50%) translateY(-50%);
     background-color: #fff;
     border: 1.5px solid var(--cl-red);
+  `,
+
+  Label: styled.p`
+    position: absolute;
+    left: 2rem;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 3rem;
+    font-weight: bold;
+
+    span {
+      color: var(--cl-red);
+    }
   `,
 }
