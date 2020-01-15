4234cf8ce7b1071823470b193bb424ade4f469da
Mathis Bullinger <bullinger.mathis@gmail.com>
2020-01-15 18:27:53 +0100

    fix: slide type annotation

--- src/context.ts
+++ src/context.ts
@@ -4,10 +4,16 @@ import slides from './slides.json'
 
 export type Language = 'en' | 'de'
 
+interface Slide {
+  question: string
+  value?: number
+  values?: { [key: string]: number } | [number, number][]
+}
+
 interface Context {
   language: Language
   strings: typeof strs.en
-  slides: any[]
+  slides: Slide[]
   setLanguage(lang: Language): void
 }
 
@@ -16,7 +22,7 @@ const language = navigator.language.startsWith('de') ? 'de' : 'en'
 export const defaultCtx: Context = {
   language,
   strings: strs[language],
-  slides,
+  slides: slides as Slide[],
   setLanguage: () => {},
 }
 
